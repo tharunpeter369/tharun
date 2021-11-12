6c4a0723ac8c90595f5cd04133e9b3e9abfaaad4
tharun <tharun.peter@lanwaresolutions.com>
2021-11-12 12:51:48 +0530

    again change

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
 
-console.clear();
+// const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require(`cli-welcome`)
+
+// clearConsole();
+
+// console.log(`
+// NAME: ${pkgJSON.name}
+// VERSION: ${pkgJSON.version}
+// DESCRIPTION: ${pkgJSON.description}`
+// );
+
+welcome({
+    title:`${pkgJSON.name}`,
+    tagline:`Get to know tharun`,
+    description:`${pkgJSON.description}`,
+    version:`${pkgJSON.version}`,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    // clear: true,
+    clear: false,
+})
+
 console.log(`Tharun Peter`)
